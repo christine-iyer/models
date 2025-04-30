65a28e35101b4da46a8996440f09f8a9258cf5e5
chrisIyer <hellohotbox420@gmail.com>
2025-04-30 11:21:46 -0400

    login not totally configured.

--- student-api/app/models/create_tables.py
+++ student-api/app/models/create_tables.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
-from models.student import Student
+from student import Student
 from sqlalchemy.ext.declarative import declarative_base
 from dotenv import load_dotenv
 import os
